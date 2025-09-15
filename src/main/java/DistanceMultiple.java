3b04b2496990dcbded6a2195db870b96ca75322f
Adam <141070325+adamsayshi@users.noreply.github.com>
2025-09-15 13:44:15 -0400

    first

--- src/main/java/DistanceMultiple.java
+++ src/main/java/DistanceMultiple.java
@@ -1,30 +1,12 @@
-/*
- * TODO: APCS-A Distance Program
- *
- * Write a program that:
- *  1. Asks the user how many inputs (pairs of points) they want to enter.
- *  2. For each input, prompt them to enter two points in the format:
- *         (x1,y1)(x2,y2)
- *     Example: (2,3)(7,3)
- *
- *  3. Use Scanner with a custom delimiter:
- *     This should ignore parentheses, commas, and spaces so only numbers remain.
- *
- *  4. Extract x1, y1, x2, y2 as doubles.
- *
- *  5. Compute the distance between the two points:
- *
- *  6. Print the results
- * 
- *  7. Repeat until all inputs have been processed.
- *
- * Example Inputs:
- *  How many inputs? 3
- *  Enter input 1 (like (x1,y1)(x2,y2)): (2,3)(7,3)
- *  Enter input 2 (like (x1,y1)(x2,y2)): (4,5)(8,8)
- *  Enter input 3 (like (x1,y1)(x2,y2)): (-1,-2)(0,7)
- *
- */
+// NAME                 : Adam Sagin
+// GROUP                : APCS-A
+// LAST MODIFIED        : 9/9/25
+// PROBLEM ID           : 2.8
+// PROBLEM DESCRIPTION  : An application that prompts for (x, y) coordinates
+//                      : and calculates and prints the distance
+// SOURCES I USED       : Java Software Solutions for AP Computer Science
+// PEOPLE I HELPED      :
+//PEOPLE WHO HELPED ME  :
 
 
 import java.util.Scanner;
@@ -32,8 +14,41 @@ import java.util.Scanner;
 
 public class DistanceMultiple {
     public static void main(String[] args) {
+        //Signature
+        System.out.println("Adam Sagin\nDistance Formula\n");
 
 
+        //Sets the scanner
+        Scanner sc = new Scanner(System.in);
+
+
+        //Get and loop number of runs
+        System.out.println("How many runs?:  ");
+        int runs = sc.nextInt();
+        sc.nextLine();
+        while (runs-- > 0) {
+            //Gets the coordinates
+            System.out.println("Enter first coordinate:  ");
+            String fcoord = sc.nextLine();
+            System.out.println("Enter second coordinate:  ");
+            String scoord = sc.nextLine();
+
+
+            //Splits the string into an array
+            String[] coords = fcoord.substring(1, fcoord.length()-1).split(",");
+            String[] scoords = scoord.substring(1, scoord.length()-1).split(",");
+
+
+            //Gets the separate x values
+            int x1 = Integer.parseInt(coords[0].trim());
+            int y1 = Integer.parseInt(coords[1].trim());
+            int x2 = Integer.parseInt(scoords[0].trim());
+            int y2 = Integer.parseInt(scoords[1].trim());
+
+
+            //Calculate and print
+            System.out.println(x1 + " " + y1 + " " + x2 + " " + y2);
+            System.out.println(Math.sqrt(Math.pow(x2-x1, 2) - Math.pow(y2-y1, 2)));
+        }
     }
-
 }
