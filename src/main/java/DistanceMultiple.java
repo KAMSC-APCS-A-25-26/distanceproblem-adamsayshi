c90530eec6eb0a5e4f64daf75225dcd605f4c13f
Adam <141070325+adamsayshi@users.noreply.github.com>
2025-09-17 18:37:56 -0400

    fix

--- src/main/java/DistanceMultiple.java
+++ src/main/java/DistanceMultiple.java
@@ -14,32 +14,21 @@ import java.util.Scanner;
 
 public class DistanceMultiple {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in).useDelimiter("[(),\\s]+");
+
         System.out.println("How many runs?:  ");
         int runs = sc.nextInt();
-        sc.nextLine();
+        System.out.println(runs);
+        //sc.nextLine();
         while (runs-- > 0) {
-            //Gets the coordinates
-            System.out.println("Enter first coordinate:  ");
-            String fcoord = sc.nextLine();
-            System.out.println("Enter second coordinate:  ");
-            String scoord = sc.nextLine();
 
-
-            //Splits the string into an array
-            String[] coords = fcoord.substring(1, fcoord.length()-1).split(",");
-            String[] scoords = scoord.substring(1, scoord.length()-1).split(",");
-
-
-            //Gets the separate x values
-            int x1 = Integer.parseInt(coords[0].trim());
-            int y1 = Integer.parseInt(coords[1].trim());
-            int x2 = Integer.parseInt(scoords[0].trim());
-            int y2 = Integer.parseInt(scoords[1].trim());
-
+            System.out.println("Enter Coordinates");
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
 
             //Calculate and print
-            System.out.println(x1 + " " + y1 + " " + x2 + " " + y2);
             System.out.println(Math.sqrt(Math.pow(x2-x1, 2) - Math.pow(y2-y1, 2)));
         }
     }
